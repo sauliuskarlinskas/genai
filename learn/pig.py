1df54779b79cb4d4c23003d3b4ce6179fc046489
sauliuskarlinskas <127085630+sauliuskarlinskas@users.noreply.github.com>
2025-03-07 19:42:41 +0200

    03.07

--- learn/pig.py
+++ learn/pig.py
@@ -20,4 +20,37 @@ while True:
     else:
         print("Invalid number, try again!")
 
-print(players)    
+print(players)   
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+
+    for player_idx in range(players):
+        print("\n Player number", player_idx + 1, "turnhas just started! \n")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll (y)?")
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+            if value ==1:
+                print("You roled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You roled a:", value)
+
+            print("Your score is:", current_score)
+
+        player_scores[player_idx] += current_score
+        print("Your total score is:", player_scores[player_idx])
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1, "is the winner with score of:", max_score)
